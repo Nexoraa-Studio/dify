991ba8486d500a854637aff47d8f4b17c4681c8f
fatelei <fatelei@gmail.com>
2026-05-14 11:06:14 +0800

    chore: add miss migration  code

---
+++ api/migrations/versions/2026_05_14_1200-1f6e6cb0f0ac_merge_recommended_apps_and_human_input_upload_heads.py
@@ -0,0 +1,22 @@
+"""merge recommended apps and human input upload heads
+
+Revision ID: 1f6e6cb0f0ac
+Revises: a4f2d8c9b731, 8d4c2a1b9f03
+Create Date: 2026-05-14 12:00:00.000000
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "1f6e6cb0f0ac"
+down_revision = ("a4f2d8c9b731", "8d4c2a1b9f03")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
